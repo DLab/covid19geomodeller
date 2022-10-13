e77b31ea20e716901afb3d740327136fb93c8b4d
samirop <samuelropert84@gmail.com>
2022-10-13 17:50:21 -0300

    Backend with debug mode on command line

--- backend/epic_backend.py
+++ backend/epic_backend.py
@@ -190,9 +190,16 @@ def datafit():
         return response, 400    
 
 
-
 if __name__ == '__main__':
-    debug = False
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--debug", action='store_true', help = "debug mode")
+    args = parser.parse_args()
+    if args.debug:
+        debug = True
+    else:
+        debug = False
+
     if debug:
         print("Initializing server in debugging mode")
         app.run(host="0.0.0.0", port=5003, debug=True)
