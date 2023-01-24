03272d77e61c9599696b730d76f3bf7c3ef11f1f
samirop <samuelropert84@gmail.com>
2023-01-24 18:10:05 -0300

    docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'cv19gm'
-copyright = '2022, Samuel Ropert'
+copyright = '2023, Samuel Ropert'
 author = 'Samuel Ropert'
 release = '0.1.0'
 
